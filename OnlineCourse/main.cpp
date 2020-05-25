de056630005659afd0d2c6a65e27697aa8ea1cf5
MARK <mark.III@live.com>
2020-05-25 09:27:18 +0800

    [ADD] main

---
+++ OnlineCourse/main.cpp
@@ -0,0 +1,11 @@
+
+
+
+#include "server.h"
+#include "window.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow) {
+	MainWindow courseWindows(hInstance);
+	Server server;
+}
+
